c14cd469831a4a75162fc0cc1010279d4e46e4bc
AllanXu <taotaoxuyu@126.com>
2020-04-13 10:08:48 +0800

    MainWin

---
+++ MainWindow.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+class Main_Windows(Frame):
+    def __init__(self,master=None):
+        Frame.__init__(self,master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.helloLabel = Label(self, text='Hello,World')
+        self.helloLabel.pack()
+        self.quitButton = Button(self, text='Quit',command=self.quit)
+        self.quitButton.pack()
+
+app = Main_Windows()
+app.master.title('Hello World')
+app.mainloop()
